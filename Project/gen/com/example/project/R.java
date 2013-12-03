da57d7bf17070b2470203e155306cd4d0d5e59c7
Brandon <Brandon@Brandon-PC>
2013-12-03 00:47:40 -0600

    ballsack.

--- Project/gen/com/example/project/R.java
+++ Project/gen/com/example/project/R.java
@@ -25,22 +25,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_save=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_launcher1=0x7f020002;
-        public static final int ic_launcher2=0x7f020003;
-        public static final int legendary_progress=0x7f020004;
-        public static final int logo=0x7f020005;
-        public static final int logo2=0x7f020006;
+        public static final int background=0x7f020000;
+        public static final int ic_action_save=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher1=0x7f020003;
+        public static final int ic_launcher2=0x7f020004;
+        public static final int legendary_progress=0x7f020005;
+        public static final int loadbuild=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int logo2=0x7f020008;
+        public static final int newbuild=0x7f020009;
     }
     public static final class id {
         public static final int action_save=0x7f090008;
         public static final int action_settings=0x7f090007;
         public static final int cancel_save=0x7f090005;
-        public static final int imageButton1=0x7f090002;
+        public static final int imageButton1=0x7f090001;
+        public static final int imageButton2=0x7f090002;
         public static final int imageView1=0x7f090000;
         public static final int save=0x7f090006;
-        public static final int textView1=0x7f090001;
         public static final int textView2=0x7f090004;
         public static final int webview=0x7f090003;
     }
--- Project/src/com/android/dawncrafter/MainActivity.java
+++ Project/src/com/android/dawncrafter/MainActivity.java
@@ -1,12 +1,18 @@
 package com.android.dawncrafter;
 
-import com.example.project.R;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ImageButton;
+
+import com.example.project.R;
 
 public class MainActivity extends Activity {
 	
@@ -15,14 +21,40 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 		
 	}
-    @Override
+    
+    ImageButton button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (ImageButton) findViewById(R.id.imageButton2);
+        registerForContextMenu(button);
     }
-
-
+    
+    public void showSaves(View v) {
+    	openContextMenu(v);
+    }
+    
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+    	menu.add(0,1,0,"Amarynth Build");
+    	menu.add(0,2,0,"Desecrator Build");
+    	menu.add(0,3,0,"Fenmore Build");
+    	menu.add(0,4,0,"Mikella Build");
+	}
+    
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        if (item.getItemId() == 1) {
+        	Log.d("SAVE1", "WOO");
+            return true;
+        } else if (item.getItemId() == 2) {
+        	Log.d("SAVE2", "YEAH");
+            return true;
+        } else {
+        	return super.onContextItemSelected(item);
+        }
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
