63704aeee5a86cb95298f7e328e5b2f618c45174
Brandon <Brandon@Brandon-PC>
2013-12-05 00:21:29 -0600

    Added playBuySound and playSellSound to javascript interface to remove sound lag on buying and selling items.

--- Project/src/com/android/dawncrafter/JavaScriptInterface.java
+++ Project/src/com/android/dawncrafter/JavaScriptInterface.java
@@ -1,9 +1,12 @@
 package com.android.dawncrafter;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
+
+import com.example.project.R;
 
 public class JavaScriptInterface {
 	Context mContext;
@@ -17,5 +20,17 @@ public class JavaScriptInterface {
 		//Save build here
 		Log.d("Test", "Test");
 		Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+	}
+	@JavascriptInterface
+	public void playBuySound() {
+		Log.d("HI", "Buy sound");
+		MediaPlayer mp = MediaPlayer.create(mContext, R.raw.buy); 
+		mp.start();
+	}
+	@JavascriptInterface
+	public void playSellSound() {
+		Log.d("HI", "Sell sound");
+		MediaPlayer mp = MediaPlayer.create(mContext, R.raw.sell); 
+		mp.start();
 	}
 }
