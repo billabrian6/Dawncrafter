003f12afacbb66a7e695e8d639e251dcfe118150
billabrian6 <billabrian6@gmail.com>
2013-12-05 19:15:02 -0600

    Added EditDialog fragment to give the user the delete/open option from saved builds.

---
+++ Project/src/com/android/dawncrafter/EditDialog.java
@@ -0,0 +1,53 @@
+package com.android.dawncrafter;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.project.R;
+
+public class EditDialog extends DialogFragment implements View.OnClickListener{
+
+	Button open, delete;
+	EditText text;
+	Communicator communicator;
+	EditText mEdit;
+	String buildname, buildurl;
+
+	@Override
+	public void onAttach(Activity activity){
+		super.onAttach(activity);
+		communicator = (Communicator) activity;
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		Bundle ofsticks = getArguments();
+		buildname = ofsticks.getString("name");
+		buildurl = ofsticks.getString("url");
+		View view = inflater.inflate(R.layout.edit_dialog, null);
+		open = (Button) view.findViewById(R.id.open);
+		delete = (Button) view.findViewById(R.id.delete);
+		open.setOnClickListener(this);
+		delete.setOnClickListener(this);
+		setCancelable(false);
+		return view;
+	}
+	@Override
+	public void onClick(View view) {
+		if(view.getId() == R.id.open){
+			communicator.onDialogSelect("open", buildname, buildurl);
+			dismiss();
+		}else{
+			communicator.onDialogSelect("delete", buildname, buildurl);
+			dismiss();
+		}
+		
+	}
+	interface Communicator{
+		public void onDialogSelect(String option, String name, String url);
+	}
+}
--- Project/src/com/android/dawncrafter/MainActivity.java
+++ Project/src/com/android/dawncrafter/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.dawncrafter;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -14,19 +16,35 @@ import android.widget.ImageButton;
 
 import com.example.project.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements EditDialog.Communicator{
 	private ArrayList<Build> builds;
 	
 	public void newBuild(View view){
 		Intent intent = new Intent(this, NewBuildActivity.class);
 		startActivity(intent);
 	}
-	
+	public void editDialog(String name, String url){
+		FragmentManager manager = getFragmentManager();
+		EditDialog dialog = new EditDialog();
+		Bundle ofsticks = new Bundle();
+		ofsticks.putString("name", name);
+		ofsticks.putString("url", url);
+		dialog.setArguments(ofsticks);
+		dialog.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+		dialog.show(manager, "EditDialog");
+	}
 	public void loadBuild(String name, String url) {
 		Intent intent = new Intent(this, NewBuildActivity.class);
 		intent.putExtra("name", name);
 		intent.putExtra("url", url);
 		startActivity(intent);
+	}
+	public void onDialogSelect(String option, String name, String url) {
+		if (option.equals("open")) {
+			loadBuild(name, url);
+		} else {
+			// Delete from sql db
+		}
 	}
     
     ImageButton button;
@@ -42,6 +60,8 @@ public class MainActivity extends Activity {
     	openContextMenu(v);
     }
     
+    
+    
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
     	BuildDataSource db = new BuildDataSource(getApplicationContext());
     	builds = db.getAllBuilds();
@@ -50,11 +70,11 @@ public class MainActivity extends Activity {
     		menu.add(0, i+1, 0, builds.get(i).getBuildName());
     	}
 	}
-    
+    // This shit gets called when you press a god damn item in the fucking context menu you piece of shit.
     public boolean onContextItemSelected(MenuItem item) {
     	for (int i = 0; i < builds.size(); i++) {
     		if (builds.get(i).getBuildName().equals(item.getTitle())) {
-    			loadBuild(builds.get(i).getBuildName(), builds.get(i).getBuildUrl());
+    			editDialog(builds.get(i).getBuildName(), builds.get(i).getBuildUrl());
     			return true;
     		}
     	}
@@ -62,6 +82,7 @@ public class MainActivity extends Activity {
     }
     
     @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
